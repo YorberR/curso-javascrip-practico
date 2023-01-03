4b7e823f08742bf7004076234141082be03c48f6
Yorber Rojas <yorberrojas15@gmail.com>
2023-01-02 22:09:41 -0400

    Carrito de compra

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.moblie-menu')
+const menuTrolleyIcon = document.querySelector('.navbar-shopping-cart')
+const menuBurgerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuBurgerIcon.addEventListener('click', toggleMobileMenu)
+menuTrolleyIcon.addEventListener('click', toggleTrolleyAside)
 
 function toggleDesktopMenu(){
+    const isAsideClosed= aside.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+
+    const isAsideClosed = aside.classList.contains('inactive')
+    
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleTrolleyAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
